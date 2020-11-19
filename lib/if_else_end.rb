6c498acd52189bb41f2554a3a1b3e902c8227e9f
charming-input-2976 <mbingramm@gmail.com>
2020-11-19 18:13:16 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
-chance_of_rain = 0.000000001
+chance_of_rain = 0.3
 if chance_of_rain > 0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  outs "Pack an umbrella!"
+  puts "Pack an umbrella!"
 else
   puts "Stay home and read Hegel."
 end
